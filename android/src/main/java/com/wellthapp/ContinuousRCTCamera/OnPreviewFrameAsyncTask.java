25bc211320cbd9adc9942872f1ac72f8980deab0
Kyle Weisel <kyleweisel@gmail.com>
2017-06-27 22:57:28 -0400

    Cleaned up the async task frame handler

--- android/src/main/java/com/wellthapp/ContinuousRCTCamera/OnPreviewFrameAsyncTask.java
+++ android/src/main/java/com/wellthapp/ContinuousRCTCamera/OnPreviewFrameAsyncTask.java
@@ -8,37 +8,29 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import clarifai2.api.ClarifaiBuilder;
-import clarifai2.api.ClarifaiClient;
-import clarifai2.api.ClarifaiResponse;
-import clarifai2.dto.input.ClarifaiImage;
-import clarifai2.dto.input.ClarifaiInput;
-import clarifai2.dto.model.output.ClarifaiOutput;
-import clarifai2.dto.prediction.Concept;
 
 import static com.wellthapp.ContinuousRCTCamera.CameraPreviewCallback.getFile;
 
 public class OnPreviewFrameAsyncTask extends AsyncTask<Void, Void, Void> {
 
-    public static class ByteArrayCameraTuple {
+    public static class CameraCaptureRequest {
         private byte[] bytes;
         private Camera camera;
+        private final ContinuousCaptureOutputConfigurations continuousCaptureOutputConfigurations;
 
-        public ByteArrayCameraTuple(final byte[] bytes, final Camera camera) {
+        public CameraCaptureRequest(final byte[] bytes, final Camera camera, final ContinuousCaptureOutputConfigurations continuousCaptureOutputConfigurations) {
             this.bytes = bytes;
             this.camera = camera;
+            this.continuousCaptureOutputConfigurations = continuousCaptureOutputConfigurations;
         }
         public byte[] getBytes() {
             return this.bytes;
@@ -46,25 +38,29 @@ public class OnPreviewFrameAsyncTask extends AsyncTask<Void, Void, Void> {
         public Camera getCamera() {
             return this.camera;
         }
+
+        public ContinuousCaptureOutputConfigurations getContinuousCaptureOutputConfigurations() {
+            return this.continuousCaptureOutputConfigurations;
+        }
+
     }
 
-    private final Promise promise;
-    private final ClarifaiClient client = new ClarifaiBuilder("99iB8g9zmTF_LHw2OfgWrpmLvh16W6IVfcILX1Gx", "IgakOLs1ijRMcfUSVTripcWJx_bBE0DDHW0GouFC").buildSync();
-    private final LinkedBlockingQueue<ByteArrayCameraTuple> queue = new LinkedBlockingQueue<>();
-    private final int positiveIdentificationTimeout;
-    private final List<String> acceptedTags;
-    private float requiredConfidence = 0.8f;
-    private int captureInterval;
+    private final ReactContext reactContext;
+    private final LinkedBlockingQueue<CameraCaptureRequest> queue = new LinkedBlockingQueue<>();
 
     private volatile boolean isRunning = false;
     private volatile boolean proceed = true;
-    private volatile boolean shouldCapture = false;
 
-    public OnPreviewFrameAsyncTask(final int captureInterval, final int positiveIdentificationTimeout, final List<String> acceptedTags, final Promise promise) {
-        this.captureInterval = captureInterval;
-        this.positiveIdentificationTimeout = positiveIdentificationTimeout;
-        this.acceptedTags = acceptedTags;
-        this.promise = promise;
+    public OnPreviewFrameAsyncTask(final ReactContext reactContext) {
+        this.reactContext = reactContext;
+    }
+
+    public final void queue(final CameraCaptureRequest cameraCaptureRequest) {
+        try {
+            queue.put(cameraCaptureRequest);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -75,39 +71,24 @@ public class OnPreviewFrameAsyncTask extends AsyncTask<Void, Void, Void> {
         while(this.proceed) {
 
             // Unpack the picture bytes
-            final ByteArrayCameraTuple byteArrayCameraTuple;
+            final CameraCaptureRequest cameraCaptureRequest;
             try {
-                byteArrayCameraTuple = queue.take();
-                final byte[] bytes = byteArrayCameraTuple.getBytes();
-                final Camera camera = byteArrayCameraTuple.getCamera();
 
+                // Unpack the byte array wrapper
+                cameraCaptureRequest = queue.take();
+                final byte[] bytes = cameraCaptureRequest.getBytes();
+                final Camera camera = cameraCaptureRequest.getCamera();
+                final ContinuousCaptureOutputConfigurations configurations = cameraCaptureRequest.getContinuousCaptureOutputConfigurations();
 
+                // Determine the camera settings
                 final Camera.Parameters parameters = camera.getParameters();
-                final int width = parameters.getPreviewSize().width;
-                final int height = parameters.getPreviewSize().height;
+                final int previewWidth = parameters.getPreviewSize().width;
+                final int previewHeight = parameters.getPreviewSize().height;
                 final int imageFormat = parameters.getPreviewFormat();
                 final int frameRate = parameters.getPreviewFrameRate();
 
-
-                final int milisecondsPerFrame = 1000 / frameRate;
-
-                // Make sure that we can't have an interval that is faster than the camera can
-                // supply frames
-                if (this.captureInterval < milisecondsPerFrame) {
-                    this.captureInterval = milisecondsPerFrame;
-                }
-
-                // Calculate the number of frames that should pass between each trigger
-                final int triggerEvery = this.captureInterval / milisecondsPerFrame;
-
-                // If it's time to trigger...
-                if (frameCounter % triggerEvery == 0) {
-                    Log.d("RCTCamera", "Time to Clarifai!");
-
-                    if(this.clarifaiImage(bytes)) {
-                        this.saveImageAndResolvePromise(bytes, width, height, promise);
-                    }
-                }
+                // Do the actual save
+                this.saveImageAndEmitEvent(bytes, previewWidth, previewHeight, configurations);
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -118,77 +99,43 @@ public class OnPreviewFrameAsyncTask extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
-    private void saveImageAndResolvePromise(final byte[] bytes, final int width, final int height, final Promise promise) {
-        final File savedImageFile = this.saveNV21Image(bytes, width, height, null);
+    private void saveImageAndEmitEvent(final byte[] bytes, final int width, final int height, final ContinuousCaptureOutputConfigurations configurations) {
+        final WritableMap event = Arguments.createMap();
 
-        final WritableMap writableMap = Arguments.createMap();
-        writableMap.putString("image", savedImageFile.getAbsolutePath());
-        promise.resolve(writableMap);
-    }
-
-    public final void queue(final byte[] bytes, final Camera camera) {
-        try {
-            this.queue.put(new ByteArrayCameraTuple(bytes, camera));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public final void stop() {
-        synchronized (this) {
-            this.isRunning = true;
-            this.proceed = false;
-        }
-        this.queue.clear();
-    }
-
-    public final boolean clarifaiImage(final byte[] bytes) {
-        final ClarifaiImage clarifaiImage = ClarifaiImage.of(bytes);
-        final ClarifaiResponse<List<ClarifaiOutput<Concept>>> response = this.client.getDefaultModels().generalModel().predict().withInputs(ClarifaiInput.forImage(clarifaiImage)).executeSync();
-        final List<ClarifaiOutput<Concept>> predictionsList = response.get();
-
-        if (response.isSuccessful()) {
-            if (!predictionsList.isEmpty()) {
-                final List<Concept> conceptList = predictionsList.get(0).data();
-
-                ClarifaiHelper.dumpConceptList(conceptList);
-
-                final Map<String, Float> conceptMap = ClarifaiHelper.toConceptMap(conceptList);
-
-                for (final String acceptedTag : this.acceptedTags) {
-                    if (conceptMap.containsKey(acceptedTag) && conceptMap.get(acceptedTag) >= this.requiredConfidence) {
-                        return true;
-                    }
-                }
-
-            } else {
-                // Predictions list was empty
-                return false;
+        if (configurations != null) {
+            final int numberOfConfigurations = configurations.getSize();
+            for (int i = 0; i < numberOfConfigurations; i++) {
+                final ContinuousCaptureOutputConfiguration configuration = configurations.getConfiguration(i);
+                final File savedFile = this.saveImageWithConfiguration(bytes, width, height, configuration);
+                event.putString(configuration.name, savedFile.getAbsolutePath());
             }
-        } else {
-            // Response was not successful
-            return false;
         }
 
-        return false;
-
+        // Actually emit the event here
+        this.reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(0, "ContinuousCaptureOutput", event);
     }
 
-
-    public File saveNV21Image(final byte[] data, int width, int height, final File customFile) {
-        Rect rect = new Rect(0, 0, width, height);
-        YuvImage img = new YuvImage(data, ImageFormat.NV21, width, height, null);
-        OutputStream outStream = null;
-        File file = (customFile != null) ? customFile : getFile();
-        try {
-            outStream = new FileOutputStream(file);
-            img.compressToJpeg(rect, 100, outStream);
-            outStream.flush();
-            outStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public File saveImageWithConfiguration(final byte[] data, int width, int height, final ContinuousCaptureOutputConfiguration configuration) {
+        if (configuration != null) {
+            final int adjustedHeight = (int)(height * configuration.height);
+            final int adjustedWidth = (int)(width * configuration.width);
+            Rect rect = new Rect(0, 0, adjustedWidth, adjustedHeight);
+            YuvImage img = new YuvImage(data, ImageFormat.NV21, adjustedWidth, adjustedHeight, null);
+            OutputStream outStream = null;
+            File file = getFile(configuration.name);
+            try {
+                outStream = new FileOutputStream(file);
+                img.compressToJpeg(rect, ((int)configuration.quality * 100), outStream);
+                outStream.flush();
+                outStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return file;
+        } else {
+            Log.w("PreviewFrameAsyncTask", "The configuration for save image was null!");
+            return null;
         }
-        return file;
     }
 
     public boolean isRunning() {
@@ -197,11 +144,20 @@ public class OnPreviewFrameAsyncTask extends AsyncTask<Void, Void, Void> {
         }
     }
 
-    public void start() {
+    public final void start() {
         synchronized (this) {
             this.isRunning = true;
         }
         this.execute();
+    }
+
+    public final void stop() {
+        synchronized (this) {
+            this.proceed = false;
+            this.isRunning = false;
+        }
+        this.queue.clear();
+        this.cancel(true);
     }
 
 }
