7b407619de0ff7f33777876d24eee933bf3dd770
Kyle Weisel <kyleweisel@gmail.com>
2017-06-27 22:56:46 -0400

    Added continuous capture output configurations

---
+++ android/src/main/java/com/wellthapp/ContinuousRCTCamera/ContinuousCaptureOutputConfigurations.java
@@ -0,0 +1,42 @@
+package com.wellthapp.ContinuousRCTCamera;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ContinuousCaptureOutputConfigurations {
+
+    public static final ContinuousCaptureOutputConfigurations getDefault() {
+        return new ContinuousCaptureOutputConfigurations(new ContinuousCaptureOutputConfiguration("thumbnail", .5d, .5d, .5d), new ContinuousCaptureOutputConfiguration("fullRes", 1d, 1d, 1d));
+    }
+
+    private final List<ContinuousCaptureOutputConfiguration> continuousCaptureOutputConfigurationList = new ArrayList<>();
+
+    private ContinuousCaptureOutputConfigurations() {
+        this.continuousCaptureOutputConfigurationList.clear();
+    }
+
+    public ContinuousCaptureOutputConfigurations(final ContinuousCaptureOutputConfiguration...configurations) {
+        Collections.copy(continuousCaptureOutputConfigurationList, Arrays.asList(configurations));
+    }
+
+    public ContinuousCaptureOutputConfiguration getConfiguration(final int index) {
+        return (index < this.continuousCaptureOutputConfigurationList.size()) ? this.continuousCaptureOutputConfigurationList.get(index) : null;
+    }
+
+    public int getSize() {
+        return this.continuousCaptureOutputConfigurationList.size();
+    }
+
+    public void clear() {
+        this.continuousCaptureOutputConfigurationList.clear();
+    }
+
+    public void addConfiguration(final ContinuousCaptureOutputConfiguration configuration) {
+        if (!this.continuousCaptureOutputConfigurationList.contains(configuration)) {
+            this.continuousCaptureOutputConfigurationList.add(configuration);
+        }
+    }
+
+}
